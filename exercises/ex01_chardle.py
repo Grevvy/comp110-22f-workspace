741b420d3453060ed17745136067583dc1caa9d5
Gerrit <ggrevenstuk@unc.edu>
2022-08-28 14:03:05 -0400

    added instance when more than one

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -15,13 +15,13 @@ count: int = (0)
 print("Searching for " + character + " in " + word)
 
 if character == word[0]:
-    print(character + " found at index 0 " )
+    print(character + " found at index 0 ")
     count = count + 1
 if character == word[1]:
-    print(character + " found at index 1 " )
+    print(character + " found at index 1 ")
     count = count + 1
 if character == word[2]:
-    print(character + " found at index 2 " )
+    print(character + " found at index 2 ")
     count = count + 1
 if character == word[3]:
     print(character + " found at index 3 ")
@@ -29,7 +29,10 @@ if character == word[3]:
 if character == word[4]:
     print(character + " found at index 4 ")
     count = count + 1
-if count > 0:
+if count == 1:
     print(str(count) + " instance of " + character + " found in " + word)
+    exit()
+if count > 1:
+    print(str(count) + " instances of " + character + " found in " + word)
 else:
-    print("No instances of " + character + " found in " + word )
+    print("No instances of " + character + " found in " + word)
