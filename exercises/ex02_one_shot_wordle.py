f44bf6f8d0e98658db20c38920a816d0c2e2703a
Gerrit <ggrevenstuk@unc.edu>
2022-09-08 22:32:03 -0400

    first autograder ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,24 +9,29 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
 a: str = ""
-exists: bool = False
-match: int = 0
+exist: bool = False
+b: int = 0
+
 
 while len(guess) != len(secret):
-    guess = str(input("That was not six letters! Try again: "))
+    guess = str(input("That was not 6 letters! Try again: "))
     
 while i < 6:
     if guess[i] == secret[i]:
         a += GREEN_BOX
     else:
-        a += WHITE_BOX
+        while exist != True and b < len(secret):
+            if secret[b] == guess[i]:
+                exist = True
+            else:
+                b += 1
+        if exist == True:
+            a += YELLOW_BOX
+        if exist == False:
+            a += WHITE_BOX
     i += 1
-
-while exists == False and match > len(secret):
-    if secret[match] == secret[match]:
-        exists == True
-    else: 
-        match += 1
+    b = 0
+    exist = False
 
 print(a)
 
