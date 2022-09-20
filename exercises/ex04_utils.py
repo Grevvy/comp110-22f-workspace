a5121ead0d35e421aa71217d10c7b3f59a458d8f
Gerrit <ggrevenstuk@unc.edu>
2022-09-19 22:00:12 -0400

    second attempt utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,10 +2,12 @@
 
 __author__ = "730529974"
 
+
 def all(numbers: list[int], compared_number: int) -> bool:
     """Determines if all the numbers in a list are equal to a number."""
     i = 0
     run_end: bool = False
+    assert len(numbers) >= 1
     while i < len(numbers) and run_end is False:
         if numbers[i] == compared_number:
             run_end = False
@@ -17,31 +19,50 @@ def all(numbers: list[int], compared_number: int) -> bool:
     else:
         return False
 
+
 def max(numbers: list[int]) -> int:
     """Determines the largest number in a list."""
-    i = 0
-    biggest: int = 0
     if len(numbers) == 0:
         raise ValueError("max() arg is an empty List")
-    while i < len(numbers) and biggest <= numbers[i]:
-        if numbers[i] > numbers[i + 1]:
-            biggest = numbers[i]
+    i: int = 1
+    a: int = 0
+    biggest: list[int] = list()
+    biggest.append(numbers[0])
+    while i < len(numbers):
+        if numbers[i] > biggest[a]:
+            biggest.append(numbers[i])
         else:
-            biggest = numbers[i + 1]
+            biggest.append(biggest[a])
         i += 1
-    return biggest
+        a += 1
+    return biggest.pop(len(biggest) - 1)
+
 
 def is_equal(input_one: list[int], input_two: list[int]) -> bool:
     """Determines if every integer in a list is equal to each element in another list."""
     i: int = 0
+    a: int = 0
+    b: int = 0
+    c: int = 0
     run_end: bool = False
-    while i < len(input_one) and run_end is False:
-        if input_one[i] == input_two[i]:
-            run_end = False
-        else:
-            run_end = True
+    run_end_two: bool = False
+    while i < len(input_one):
+        while a < len(input_two) and run_end is False:
+            if input_one[i] == input_two[a]:
+                run_end = False
+            else:
+                run_end = True
+            a += 1
         i += 1
-    if run_end is True:
+    while b < len(input_two):
+        while c < len(input_one) and run_end_two is False:
+            if input_two[b] == input_one[c]:
+                run_end = False
+            else:
+                run_end = True
+            c += 1
+        b += 1
+    if run_end or run_end_two is True:
         return False
     else:
         return True
@@ -49,7 +70,8 @@ def is_equal(input_one: list[int], input_two: list[int]) -> bool:
 
 def main() -> None:
     """Testing for the list utility functions."""
-    print(is_equal([1, 0, 1], [1, 0, 1]))
+    print(is_equal([1, 1, 0], [1, 0, 1]))
+
 
 if __name__ == "__main__":
     main()
