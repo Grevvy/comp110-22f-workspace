06a3349374920bffbfbe20356d266090524ad873
Gerrit <ggrevenstuk@unc.edu>
2022-11-16 19:11:57 -0500

    fixed methods, added comments.

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -11,17 +11,21 @@ class Simpy:
     values: list[float]
 
     def __init__(self, values: list[float]):
+        """Initializes a simpy."""
         self.values = values
 
     def __repr__(self) -> str:
+        """A representation of a simpy."""
         return f"Simpy({self.values})"
     
-    def fill(self, fill_value: float, number_of_values: int):
+    def fill(self, fill_value: float, number_of_values: int) -> None:
+        """Fills a simpy with the value specified and the number of times."""
         self.values = []
         for _ in range(0, number_of_values):
             self.values.append(fill_value)
 
-    def arange(self, start: float, stop: float, step: float = 1.0):
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        """Fills a simpy with values from a start and stop at a designated interval."""
         assert step != 0.0
         value: float = start
         while value != stop:
@@ -29,9 +33,11 @@ class Simpy:
             value += step
     
     def sum(self) -> float:
+        """Sums the values in a simpy."""
         return sum(self.values)
 
     def __add__(self, rhs: Union[Simpy, float]) -> Simpy:
+        """Adds a value to every value in a simpy and also adds two simpys together."""
         result: Simpy = Simpy([])
         if isinstance(rhs, float):
             for value in self.values:
@@ -43,6 +49,7 @@ class Simpy:
         return result
 
     def __pow__(self, rhs: Union[Simpy, float]) -> Simpy:
+        """Raises every value in a simpy to a specified power and also allows for two simpys to be raised to each other."""
         result: Simpy = Simpy([])
         if isinstance(rhs, float):
             for value in self.values:
@@ -54,6 +61,7 @@ class Simpy:
         return result
 
     def __eq__(self, rhs: Union[Simpy, float]) -> list[bool]:
+        """Determines which values in a simpy are equal to values in another simpy, also if values in a simpy are equal to a specified float value."""
         result: list[bool] = []
         if isinstance(rhs, float):
             for value in self.values:
@@ -65,6 +73,7 @@ class Simpy:
         return result
 
     def __gt__(self, rhs: Union[Simpy, float]) -> list[bool]:
+        """Determines is values in a simpy are greater than a specified float value. Also determines if one simpy is greater than another simpy."""
         result: list[bool] = []
         if isinstance(rhs, float):
             for value in self.values:
@@ -76,6 +85,7 @@ class Simpy:
         return result
 
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Uses a mask to get the values desired in a simpy. Can also be used to retrieve values that match a given argument."""
         result: Simpy = Simpy([])
         if isinstance(rhs, list):
             assert len(self.values) == len(rhs)
